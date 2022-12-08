0ce5617c81c4c022c58e4c068e3003b7d9c331db
Alice Doe <pnacht+alice@google.com>
2022-12-08 14:56:45 +0000

    Rewrite cron_to_json()

--- src/cronk/cron_to_json.py
+++ src/cronk/cron_to_json.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from io import TextIOWrapper
+import json
 import re
 
 
@@ -9,6 +9,13 @@ class Command:
     time: str = None
     command: str = None
 
+    def __init__(self, comments: list[str], command: str):
+        self.comments = comments
+
+        m = re.match(r"((?:[^ ]* ){5})(.*)", command)
+        self.time = m.group(1).rstrip()
+        self.command = m.group(2)
+
 
 @dataclass
 class Json:
@@ -16,29 +23,33 @@ class Json:
     commands: list[Command] = field(default_factory=list)
     outro: list[str] = field(default_factory=list)
 
-    def to_dict(self):
-        return {"intro": self.intro, "commands": self.commands, "outro": self.outro}
+    def __repr__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
 
-def is_comment(s: str) -> bool:
-    return bool(re.search("^ *#+ *", s))
+def _is_command(s: str) -> bool:
+    return s != "" and not bool(re.search("^ *#", s))
 
 
-def get_comment_block(file: TextIOWrapper) -> list[str]:
-    pos = file.tell()
-    block: list[str] = []
-    while file:
-        line = file.readline()
+def _split_comments(lines: list[str], command_idx: list[int]):
+    end_of_intro = 0
+    for i, line in enumerate(lines[: command_idx[0]]):
+        if line == "":
+            end_of_intro = i
 
-        if not is_comment(line):
-            file.seek(pos)
-            return block
+    intro = lines[:end_of_intro]
 
-        block.append(line)
-        pos = file.tell()
+    command_comments = [
+        lines[(start + 1) : end]
+        for start, end in zip([end_of_intro] + command_idx, command_idx + [len(lines)])
+    ]
+
+    outro = command_comments.pop()
+
+    return intro, command_comments, outro
 
 
-def cron_to_json(file: TextIOWrapper) -> dict:
+def cron_to_json(lines: list[str]) -> dict:
     """
     Converts a cron file to a json file.
 
@@ -91,4 +102,26 @@ def cron_to_json(file: TextIOWrapper) -> dict:
     ```
     """
 
-    pass
+    line_types = [_is_command(line) for line in lines]
+
+    command_idx = [i for i, line in enumerate(line_types) if line]
+    commands = [lines[i] for i in command_idx]
+
+    if not commands:  # "empty" cron file, no commands
+        return Json(intro=lines)
+
+    intro, command_comments, outro = _split_comments(lines, command_idx)
+
+    commands = [
+        Command(comment, command)
+        for comment, command in zip(command_comments, commands)
+    ]
+
+    return Json(intro=intro, commands=commands, outro=outro)
+
+
+if __name__ == "__main__":
+    with open("src/cronk/test.txt") as f:
+        lines = [s.rstrip() for s in f.readlines()]
+
+    print(cron_to_json(lines))
