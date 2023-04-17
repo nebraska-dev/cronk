941b6612b5de3f9aa9f898b4dbe9185f09d08d0f
Alice Doe <pnacht+alice@google.com>
2023-04-17 20:01:32 +0000

    Add pytest unit tests

--- src/cronk/cron_to_json.py
+++ src/cronk/cron_to_json.py
@@ -109,8 +109,3 @@ def _split_comments(
     outro = command_comments.pop()
 
     return intro, command_comments, outro
-
-
-if __name__ == "__main__":
-    with open("src/cronk/test.txt") as f:
-        print(cron_to_json(f))
