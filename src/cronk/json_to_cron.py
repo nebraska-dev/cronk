5991c3a42c88f2d280006183b126b49670d311ef
Alice Doe <pnacht+alice@google.com>
2023-04-17 19:59:26 +0000

    Adopt Json and Routine in json_to_cron

--- src/cronk/json_to_cron.py
+++ src/cronk/json_to_cron.py
@@ -3,27 +3,42 @@ from io import TextIOWrapper
 
 from loguru import logger
 
-
-def _routine_to_cron(routines: list[dict[str, str]]) -> list[str]:
-    return [
-        s for routine in routines for s in routine["comments"] + [routine["command"]]
-    ]
+from cronk.json_routine import Json, Routine
 
 
 def json_to_cron(fp: TextIOWrapper) -> list[str]:
     logger.debug(f"Converting {fp.name} to cron format")
-    js = json.load(fp)
+    js = _to_Json(json.load(fp))
 
-    output = js["intro"]
+    output = js.intro
 
-    if not js["routines"]:
+    if not js.routines:
         return output
 
     output.append("")  # blank line between intro and first command
-    output.extend(_routine_to_cron(js["routines"]))
-    output.extend(js["outro"])
+    output.extend(_routine_to_cron(js.routines))
+    output.extend(js.outro)
 
     return output
+
+
+def _routine_to_cron(routines: list[Routine]) -> list[str]:
+    return [
+        s
+        for routine in routines
+        for s in routine.comments + [routine.time + " " + routine.command]
+    ]
+
+
+def _to_Json(json: dict) -> Json:
+    return Json(
+        intro=json["intro"],
+        routines=[
+            Routine(comments=r["comments"], command=r["command"])
+            for r in json["routines"]
+        ],
+        outro=json["outro"],
+    )
 
 
 if __name__ == "__main__":
