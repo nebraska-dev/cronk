581d50362057a681858e9b31310ce31fb085c6ae
Alice Doe <pnacht+alice@google.com>
2023-04-17 20:01:32 +0000

    Add pytest unit tests

---
+++ tests/json_to_cron_test.py
@@ -0,0 +1,20 @@
+from cronk.json_to_cron import json_to_cron
+
+
+def test_cron_to_json_handles_happy_path():
+    with open("tests/json_to_cron_test_happy.json") as fp:
+        text = fp.read()
+
+    assert json_to_cron(text) == [
+        "# Intro comment",
+        "",
+        "# First action comment",
+        "0 * * * * echo 'hello world'",
+        "# Second action",
+        "# comment",
+        "1 * * * * echo 'farewell'",
+        "# Outro",
+    ]
+
+
+# TODO: Test sad path
